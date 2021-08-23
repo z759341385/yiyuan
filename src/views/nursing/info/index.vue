1a39c4156baf04285109c206f7a661d179b2ada5
王多鱼 <xbb921@163.com>
2021-08-23 11:07:15 +0800

    优化

--- src/views/nursing/info/index.vue
+++ src/views/nursing/info/index.vue
@@ -5,37 +5,37 @@
       <div class="title_box flex am_c">
         <div v-for="item in titleList" :key="item.label" class="flex col am_c title_item" :style="item.style">
           <div>{{ item.label }}</div>
-          <div>{{ item.eng }}</div>
+          <!-- <div>{{ item.eng }}</div> -->
         </div>
       </div>
-      <div class="list_box">
-        <div v-for="item in operation.list" :key="item.label" class="flex am_c list_item">
-          <div class="t_ct" :style="titleList[0].style">{{ item.id }}</div>
-          <div class="t_ct" :style="titleList[1].style">{{ item.name }}</div>
-          <div class="t_ct" :style="titleList[2].style">{{ item.sex }}</div>
-          <div class="t_ct" :style="titleList[3].style">{{ item.age }}</div>
-          <div class="t_ct" :style="titleList[4].style">{{ item.eventName }}</div>
-          <div class="t_ct" :style="titleList[5].style">{{ item.eventDate }}</div>
-          <div class="t_ct" :style="titleList[5].style">{{ item.eventDate }}</div>
-          <div class="t_ct" :style="titleList[5].style">{{ item.eventDate }}</div>
+      <vue-seamless-scroll :data="operation.list" class="list_box" :class-option="classOption">
+        <div>
+          <div v-for="item in operation.list" :key="item.label" class="flex am_c list_item">
+            <div class="t_ct" :style="titleList[0].style">{{ item.id }}</div>
+            <div class="t_ct" :style="titleList[1].style">{{ item.name }}</div>
+            <div class="t_ct" :style="titleList[2].style">{{ item.sex }}</div>
+            <div class="t_ct" :style="titleList[3].style">{{ item.age }}</div>
+            <div class="t_ct" :style="titleList[4].style">{{ item.nursingGrade }}</div>
+            <div class="t_ct" :style="titleList[5].style">{{ item.illnessState }}</div>
+            <div class="t_ct" :style="titleList[5].style">{{ item.allergyInfo }}</div>
+            <div class="t_ct" :style="titleList[5].style">{{ item.dietType }}</div>
+          </div>
         </div>
-      </div>
+      </vue-seamless-scroll>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-
 import { Component, Vue } from "vue-property-decorator";
 import Header from "@/components/Header.vue";
-const operation = require("@/assets/nursing_operation.js").json;
-
+import vueSeamlessScroll from "vue-seamless-scroll";
+const operation = require("@/assets/nursing_info.js").json;
 
 @Component({
-  name: "SearchPopup",
-  components: {Header},
+  name: "NursingInfo",
+  components: { Header, vueSeamlessScroll },
 })
-
 export default class extends Vue {
   operation = operation;
 
@@ -49,6 +49,17 @@ export default class extends Vue {
     { label: "过敏信息", eng: "allergyInfo", style: "width: 240px" },
     { label: "饮食类型", eng: "dietType", style: "width: 240px" },
   ];
+
+  classOption = {
+    step: 1, // 数值越大速度滚动越快
+    limitMoveNum: 10, // 开始无缝滚动的数据量 this.dataList.length
+    hoverStop: true, // 是否开启鼠标悬停stop
+    direction: 1, // 0向下 1向上 2向左 3向右
+    openWatch: true, // 开启数据实时监控刷新dom
+    singleHeight: 51, // 单步运动停止的高度(默认值0是无缝不停止的滚动) direction => 0/1
+    singleWidth: 0, // 单步运动停止的宽度(默认值0是无缝不停止的滚动) direction => 2/3
+    waitTime: 1000, // 单步运动停止的时间(默认值1000ms)
+  };
 }
 </script>
 <style lang="less" scoped>
@@ -61,6 +72,10 @@ export default class extends Vue {
     }
   }
   .list_box {
+    height: calc(100vh - 171px);
+    overflow: hidden;
+    box-shadow: 0 0 10px #5389e2 inset;
+    border-radius: 8px;
     .list_item {
       padding: 15px 0;
     }
